f2725f28fcbd730afb9d514a74bbe08d4360886f
vishwajeet kumar <39018958+vishusrivastva@users.noreply.github.com>
2021-07-22 23:02:02 +0530

    Add files via upload

---
+++ Assignment 1/promoRouter.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const promoRouter = express.Router();
+
+promoRouter.use(bodyParser.json());
+
+promoRouter.route('/')
+.all((req,res,next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req,res,next) => {
+    res.end('Will send all the promotions to you!');
+})
+.post((req, res, next) => {
+    res.end('Will add the promotion: ' + req.body.name + ' with details: ' + req.body.description);
+})
+.put((req, res, next) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /promotions');
+})
+.delete((req, res, next) => {
+    res.end('Deleting all promotions');
+    next();
+});
+
+promoRouter.route('/:promoId')
+.all(function (req, res, next) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    next();
+})
+
+.get(function (req, res, next) {
+    res.end('Will send details of the promotion: ' + req.params.promoId + ' to you!');
+})
+
+.put(function (req, res, next) {
+    res.write('Updating the promotion: ' + req.params.dishId + '\n');
+    res.end('Will update the promotion: ' + req.body.name +
+        ' with details: ' + req.body.description);
+})
+.post((req, res, next) => {
+    res.statusCode = 403;
+    res.end('POST operation not supported on /promotion/'+ req.params.promoId);
+})
+.delete(function (req, res, next) {
+    res.end('Deleting promotion: ' + req.params.promoId);
+});
+
+module.exports = promoRouter;
